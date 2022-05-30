67ac234892e6d9b4e3fed77cdbebfc7b5e00b15c
Vivek kumar <viveksinghssm496@gmail.com>
2022-05-30 08:58:16 +0530

    Added to main

---
+++ HashTable/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\tWelcome to Hash Map Program");
+        }
+    }
+}
